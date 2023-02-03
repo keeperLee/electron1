cc951cfbd1039996293db84afbd5ae9558928878
lijian <lijian@52tt.com>
2023-02-03 14:37:50 +0800

    创建BrowserWindow窗口

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log(111)
+let electron = require("electron");
+let app = electron.app;
+let {BrowserWindow} = electron;
+let win = null;
+app.on("ready", function () {
+  win = new BrowserWindow({
+    webPreferences: { nodeIntegration: true },  //允许页面集成node环境
+  });
+  win.loadFile('index.html')
+  win.on('closed',function(){
+    win = null
+  })
+});
+
+app.on('window-all-closed',function(){
+    app.quit()
+})
