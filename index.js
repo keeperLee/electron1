7561d0457cfc36c0b38ec3ea2c403ce1d452bb2d
lijian <lijian@52tt.com>
2023-02-03 19:00:03 +0800

    feat: 渲染进程通过remote访问主进程对象类型

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 let electron = require("electron");
-let {app,BrowserWindow} = electron;
+let { app, BrowserWindow } = electron;
 let win = null;
 app.on("ready", function () {
   win = new BrowserWindow({
-    webPreferences: { nodeIntegration: true },  //允许页面集成node环境
+    webPreferences: { nodeIntegration: true }, //允许页面集成node环境
   });
-  win.loadFile('index.html')
-  win.webContents.openDevTools() //打开调试面板
-  win.on('closed',function(){
-    win = null
-  })
+  win.loadFile("index.html");
+  //   win.webContents.openDevTools() //打开调试面板
+  win.on("closed", function () {
+    win = null;
+  });
 });
 
-app.on('window-all-closed',function(){
-    app.quit()
-})
+app.on("window-all-closed", function () {
+  app.quit();
+});
