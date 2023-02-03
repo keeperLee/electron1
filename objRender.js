215c0eb5660d35aa6c6beaba8394a088b1f74670
lijian <lijian@52tt.com>
2023-02-03 15:46:36 +0800

    feat: 添加require的版本对比

--- objRender.js
+++ objRender.js
@@ -1,3 +1,4 @@
-let objRender2 = require('./objRender2')
+//使用8.1.0版本electron的时候，可以通过window对象获取到require引入的内容，但是在22.2.0版本不行
+let objRender2 = require('./objRender2')  
 window.objRender = { key: "value1" };
 window.objRender2 = objRender2
