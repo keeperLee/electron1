b5c1a691c45da086b12cc691cd6358e7bc534eef
lijian <lijian@52tt.com>
2023-02-06 14:16:35 +0800

    feat: 添加vue.config.js的插件配置开关

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,71 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+    <div id="app">
+        <div class="titleBar">
+            <div class="title">
+                <div class="logo"><img src="@/assets/logo.png" alt=""></div>
+                <div class="txt">窗口标题</div>
+            </div>
+            <div class="windowTool">
+                <div @click="minisize">
+                    <i class="iconfont iconminisize"></i>
+                </div>
+                <div v-if="isMaxSize" @click="restore">
+                    <i class="iconfont iconrestore"></i>
+                </div>
+                <div v-else  @click="maxsize">
+                    <i class="iconfont iconmaxsize"></i>
+                </div>
+                <div class="close" @click="close">
+                    <i class="iconfont iconclose"></i>
+                </div>
+            </div>
+        </div>
+        <div class="content">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body,html{
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    height: 100%;
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+#app {
+    text-align: center;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    overflow: hidden;
+    box-sizing: border-box;
+    border: 1px solid #f5222d;
+    flex-direction: column;
 }
 </style>
+
+<script>
+let {remote} = require("electron")
+export default{
+    data(){
+        return {
+            isMaxSize:false
+        }
+    },
+    methods:{
+        close(){
+            remote.getCurrentWindow.close()
+        },
+        minisize(){
+            remote.getCurrentWindow.minisize()
+        },
+        restore(){
+            remote.getCurrentWindow.restore()
+        },
+        maxsize(){
+            remote.getCurrentWindow.maxsize()
+        }
+    }
+}
+</script>
