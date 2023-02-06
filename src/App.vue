00b57cb2dfc14e6125cf7dc1a6691a891e8d94a3
lijian <lijian@52tt.com>
2023-02-06 17:00:58 +0800

    feat: 联调remote的api

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
                 <div v-if="isMaxSize" @click="restore">
                     <i class="iconfont iconrestore"></i>
                 </div>
-                <div v-else  @click="maxsize">
+                <div v-else @click="maxsize">
                     <i class="iconfont iconmaxsize"></i>
                 </div>
                 <div class="close" @click="close">
@@ -21,13 +21,14 @@
             </div>
         </div>
         <div class="content">
-            <router-view/>
+            <router-view />
         </div>
     </div>
 </template>
 
 <style lang="scss">
-body,html{
+body,
+html {
     margin: 0;
     padding: 0;
     overflow: hidden;
@@ -47,20 +48,20 @@ body,html{
 
 <style lang="scss" scoped>
 @import url(https://at.alicdn.com/t/font_1378132_s4e44adve5.css);
-.titleBar{
+.titleBar {
     height: 38px;
     line-height: 36px;
     background: #fff1f0;
     display: flex;
-    border-bottom:1px solid #f5222d;
-    .title{
-        flex:1;
+    border-bottom: 1px solid #f5222d;
+    .title {
+        flex: 1;
         display: flex;
-        -webkit-app-region:drag;
-        .logo{
+        -webkit-app-region: drag;
+        .logo {
             padding-left: 8px;
             padding-right: 6px;
-            img{
+            img {
                 width: 20px;
                 height: 20px;
                 margin-top: 7px;
@@ -68,58 +69,105 @@ body,html{
         }
         .txt {
             text-align: left;
-            flex:1;
+            flex: 1;
         }
     }
 }
-.windowTool{
+.windowTool {
     div {
-        color:#888;
-        height:100%;
-        width:38px;
+        color: #888;
+        height: 100%;
+        width: 38px;
         display: inline-block;
         cursor: pointer;
-        &:hover{
+        &:hover {
             background: #ffccc7;
         }
-        i{
+        i {
             font-size: 12px;
         }
     }
-    .close:hover{
-        color:#fff;
+    .close:hover {
+        color: #fff;
         background: #ff4d4f;
     }
 }
-.content{
-    flex:1;
-    overflow-y:auto;
-    overflow-x:auto;
+.content {
+    flex: 1;
+    overflow-y: auto;
+    overflow-x: auto;
 }
 </style>
 
 <script>
-export default{
-    data(){
+let { remote } = require('electron')
+export default {
+    data () {
         return {
-            isMaxSize:false
+            isMaxSize: false,
         }
     },
-    methods:{
-        close(){
-            // remote.getCurrentWindow.close()
+    methods: {
+        close () {
+            console.log(remote)
+            remote.getCurrentWindow().close()
         },
-        minisize(){
-            // remote.getCurrentWindow.minisize()
+        minisize () {
+            remote.getCurrentWindow().minimize()
         },
-        restore(){
-            // remote.getCurrentWindow.restore()
+        restore () {
+            remote.getCurrentWindow().restore()
         },
-        maxsize(){
-            // remote.getCurrentWindow.maxsize()
+        maxsize () {
+            remote.getCurrentWindow().maximize()
+        },
+        debounce (fn) {
+            let timeout = null;
+            return function () {
+                clearTimeout(timeout);
+                timeout = setTimeout(() => {
+                    fn.apply(this, arguments)
+                }, 300)
+            }
+        },
+        setState(){
+            let win = remote.getCurrentWindow();
+            let rect = win.getBounds();
+            let isMaxSize = win.isMaximized();
+            let obj = {rect,isMaxSize};
+            localStorage.setItem('winState',JSON.stringify(obj))
+        },
+        getState(){
+            let win = remote.getCurrentWindow();
+            let winState = localStorage.getItem('winState');
+            if(winState){
+                winState = JSON.parse(winState);
+                if(winState.isMaxSize){
+                    win.maximize();
+                }else{
+                    win.setBounds(winState.rect);
+                }
+            }
         }
     },
-    created(){
+    mounted () {
+        let win = remote.getCurrentWindow();
+        win.on('maximize', () => {
+            this.isMaxSize = true;
+            this.setState()
+        });
+        win.on('unmaximize', () => {
+            this.isMaxSize = false;
+            this.setState()
+        });
+        win.on('move', this.debounce(() => {
+            this.setState()
+        }));
+        win.on('resize', this.debounce(() => {
+            this.setState()
+        }));
+        this.isMaxSize = win.isMaximized()
+        this.getState();
     }
 }
 </script>
